4e31b9599ae21b6f25378fc43e97063c12fc7233
maquino96 <matthewaquino96@gmail.com>
2021-03-22 19:45:50 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ function App() {
   const [listings, setListings] = useState([])
   const [ searchTerm, setSearchTerm] = useState('')
   const [filter, setFilter] = useState([])
-  // I've come to the same conclusion twice so far that the filterArray is required when creating the search bar feature. 
-  console.log(searchTerm)
 
-  // console.log(listings)
+  const [sortList, setSortList] = useState('')
+
+  // I've come to the same conclusion twice so far that the filterArray is required when creating the search bar feature. 
+  // console.log(searchTerm)
+
+  console.log(listings)
 
   useEffect(() => {
     fetchAPI()
@@ -37,9 +40,44 @@ function App() {
 
   }
 
+  const handleSort = (event) => {
+    event.preventDefault()
+    setSortList(event.target.value)
+
+    switch (sortList){
+      case 'Default':
+        return console.log('default')
+      case 'Location':
+        setListings(listings.sort( (a, b) => {
+          if ( a.location < b.location ){
+              return -1;
+          }
+          if ( a.location > b.location ){
+              return 1;
+          }
+              return 0;
+          }
+        ))
+        return fetchAPI()
+      case 'Price':
+        return console.log('price')
+      default:
+        break 
+    }
+
+
+  }
+
+
   return (
     <div className="app">
       <Header searchTerm={searchTerm} onSearch={setSearchTerm} handleSearch={handleSearch}/>
+        <label htmlFor="sort">Sort Listings</label> <br></br>
+        <select name="sort-by" id="sort-dropdown" onChange={e => handleSort(e)}>
+        <option value="Default">Default</option>
+          <option value="Location">Location</option>
+          <option value="Price">Price</option>
+        </select>
       <ListingsContainer listings={searchTerm ? filter: listings} onDelete={handleDelete} />
     </div>
   );
